25e5be9487e580bff5b610dceeb1256caa8efcbc
anhtu12st <anhtu12st@gmail.com>
2021-12-19 19:34:39 +0700

    Singleton Design Principle

---
+++ Section 5/excercise.py
@@ -0,0 +1,16 @@
+# function to check a factory method is singleton
+from unittest import TestCase
+from copy import deepcopy
+
+
+def is_singleton(factory):
+    x = factory()
+    y = factory()
+    return x is y
+
+
+class Evaluate(TestCase):
+    def test_exercise(self):
+        obj = [1, 2, 3]
+        self.assertTrue(is_singleton(lambda: obj))
+        self.assertFalse(is_singleton(lambda: deepcopy(obj)))
---
+++ Section 5/readme.md
@@ -0,0 +1,6 @@
+# Singleton Pattern
+- Different realizations of Singleton: custom allocator, decorator, metaclass
+- Laziness is easy, just init on first request (heavy init)
+- Monostate variation
+- Testability issues
+
